3f642d8b633aadd46db78d6e078997757bc071bd
1656492952 <113780035+1656492952@users.noreply.github.com>
2022-09-18 09:00:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -78,8 +78,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25"}
-        //,
+        {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25"},
         //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
@@ -88,8 +87,7 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2021-10-28"}
-        //,
+        {"keyword": "love_day", date: "2021-10-28"},
         // 结婚纪念日
         //{"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
@@ -105,8 +103,8 @@ export const config = {
       horoscopeDate: '12-28',
       horoscopeDateType: '今日',
       openUrl: "",
-      festivals: [ {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25"}],
-      customizedDateList: [ {"keyword": "love_day", date: "2021-10-28"}]
+      festivals: [ {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25"},],
+      customizedDateList: [ {"keyword": "love_day", date: "2021-10-28"},]
     },
     {
       name: "老婆2",
@@ -187,8 +185,7 @@ export const config = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25", isShowAge: true}
-    //,
+    {"type": "*生日", "name": "宝贝", "year": "2001", "date": "06-25", isShowAge: true},
     //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
@@ -212,8 +209,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"}
-    //,
+    {"keyword": "love_day", date: "2022-09-08"},
     // 结婚纪念日
    //{"keyword": "marry_day", date: "2022-09-09"},
     // 退伍日
